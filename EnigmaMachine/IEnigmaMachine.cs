644a0ed84db597c5be76170cd2ee56060bce9fe2
Stephane Larocque <stephl001@gmail.com>
2015-06-11 12:58:43 -0400

    Added public interface definition

---
+++ EnigmaMachine/IEnigmaMachine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnigmaMachine
+{
+    public interface IEnigmaMachine
+    {
+        char PressKey(char letter);
+        void SetupPlugboard(string mappings);
+        void SetupRotors(RotorInfo[] rotorInfos);
+        void SetupReflector(string type);
+        char[] GetCurrentRotorRingLetters();
+        void ResetRotors();
+    }
+}
---
+++ EnigmaMachine/RotorInfo.cs
@@ -0,0 +1,16 @@
+﻿namespace EnigmaMachine
+{
+    public sealed class RotorInfo
+    {
+        public RotorInfo(string type, char startingOffset = 'A', char ringSettingOffset = 'A')
+        {
+            Type = type;
+            RingSettingOffset = ringSettingOffset;
+            StartingOffset = startingOffset;
+        }
+
+        public string Type { get; private set; }
+        public char RingSettingOffset { get; private set; }
+        public char StartingOffset { get; private set; }
+    }
+}
